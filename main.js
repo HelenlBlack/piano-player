066c03e19b7edf9dd3fcabd6c7db0c18d2e8473e
Helen Black <27014723+HelenlBlack@users.noreply.github.com>
2022-05-09 19:36:02 +0100

    Added sound to piano player

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
 // The keys and notes variables store the piano keys
 const keys = ['c-key', 'd-key', 'e-key', 'f-key', 'g-key', 'a-key', 'b-key', 'high-c-key', 'c-sharp-key', 'd-sharp-key', 'f-sharp-key', 'g-sharp-key', 'a-sharp-key'];
 const notes = [];
+const datanotes = ['C', 'Cs', 'D', 'Ds', 'E', 'F', 'Fs', 'G', 'Gs', 'A', 'As', 'B', 'hC'];
+
 var i;
+//var audio = new Audio('./resources/C.mp3');
+
 keys.forEach(function(key){
   notes.push(document.getElementById(key));
 })
 console.log('notes is:', notes);
 
+
+
 // Write named functions that change the color of the keys below
 
 function keyPlay(){
   this.style.backgroundColor = 'green';
+  let note = this.dataset.key;
+  console.log('this.dataset.key', this.dataset.key);
+  //const notetoplay = document.getElementById(note);
+  //const audiofile = notetoplay.getAttribute('src');
+  //console.log('document.getElementById(note)', document.getElementById(note));
+  //document.getElementById(note).play();
+  let audiofile = './resources/' + note + '.mp3';
+  //audio.setAttribute('src', audiofile);
+  //console.log('audiofile', audiofile);
+  //const filetoplay = audiofile.getAttribute('src');
+  //console.log('filetoplay', filetoplay);
+  var audio = new Audio(audiofile);
+  //audio.src = audiofile;
+  console.log('audio.src', audio.src);
+  audio.play();
 }
 
 function keyReturn(){
@@ -25,8 +46,6 @@ for (i=0; i < notes.length; i++) {
   notes[i].addEventListener('mousedown', keyPlay);
   notes[i].addEventListener('mouseup', keyReturn);
 }
-
-//notes.forEach(e => {listenerAssignment(e);});
 
 // These variables store the buttons that progress the user through the lyrics
 let nextOne = document.getElementById('first-next-line');
@@ -43,12 +62,60 @@ nextThree.hidden = true;
 startOver.hidden= true;
 
 // Write anonymous event handler property and function for the first progress button
-
+nextOne.onclick = function() {
+  nextTwo.hidden = false;
+  nextOne.hidden = true;
+   document.getElementById('word-one').innerHTML = 'HAP-';
+  document.getElementById('letter-note-one').innerHTML = 'G';
+  document.getElementById('word-two').innerHTML = 'PY';
+  document.getElementById('letter-note-two').innerHTML = 'G';
+  document.getElementById('word-three').innerHTML = 'BIRTH-';
+  document.getElementById('letter-note-three').innerHTML = 'A';
+  document.getElementById('word-four').innerHTML = 'DAY';
+  document.getElementById('letter-note-four').innerHTML = 'G';
+  document.getElementById('word-five').innerHTML = 'TO';
+  document.getElementById('letter-note-five').innerHTML = 'D';
+  document.getElementById('word-six').innerHTML = 'YOU!';
+  document.getElementById('letter-note-six').innerHTML = 'C';
+}
 
 // Write anonymous event handler property and function for the second progress button
-
+nextTwo.onclick = function() {
+  nextThree.hidden = false;
+  nextTwo.hidden = true;
+  document.getElementById('column-optional').style.display = 'inline-block';
+   document.getElementById('word-one').innerHTML = 'HAP-';
+  document.getElementById('letter-note-one').innerHTML = 'G';
+  document.getElementById('word-two').innerHTML = 'PY';
+  document.getElementById('letter-note-two').innerHTML = 'G';
+  document.getElementById('word-three').innerHTML = 'BIRTH-';
+  document.getElementById('letter-note-three').innerHTML = 'G';
+  document.getElementById('word-four').innerHTML = 'DAY';
+  document.getElementById('letter-note-four').innerHTML = 'E';
+  document.getElementById('word-five').innerHTML = 'DEAR';
+  document.getElementById('letter-note-five').innerHTML = 'C';
+  document.getElementById('word-six').innerHTML = 'FRI';
+  document.getElementById('letter-note-six').innerHTML = 'B';
+}
 
 // Write anonymous event handler property and function for the third progress button
+nextThree.onclick = function() {
+  startOver.hidden = false;
+  nextThree.hidden = true;
+  document.getElementById('column-optional').style.display = 'none';
+   document.getElementById('word-one').innerHTML = 'HAP-';
+  document.getElementById('letter-note-one').innerHTML = 'F';
+  document.getElementById('word-two').innerHTML = 'PY';
+  document.getElementById('letter-note-two').innerHTML = 'F';
+  document.getElementById('word-three').innerHTML = 'BIRTH-';
+  document.getElementById('letter-note-three').innerHTML = 'E';
+  document.getElementById('word-four').innerHTML = 'DAY';
+  document.getElementById('letter-note-four').innerHTML = 'C';
+  document.getElementById('word-five').innerHTML = 'TO';
+  document.getElementById('letter-note-five').innerHTML = 'D';
+  document.getElementById('word-six').innerHTML = 'YOU!';
+  document.getElementById('letter-note-six').innerHTML = 'C';
+}
 
 
 // This is the event handler property and function for the startOver button
